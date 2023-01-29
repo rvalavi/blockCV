efd2c63bd564c59180396eb054d5ad97dcf1a5d2
Roozbeh Valavi <valavi.r@gmail.com>
2023-01-29 19:15:00 +1100

    vignettes

--- R/buffering.R
+++ R/buffering.R
@@ -16,16 +16,6 @@
 #'
 #' @seealso \code{\link{cv_buffer}}
 #'
-#' @return An object of class S3. A list of objects including:
-#'     \itemize{
-#'     \item{folds - a list containing the folds. Each fold has two vectors with the training (first) and testing (second) indices}
-#'     \item{k - number of the folds}
-#'     \item{range - the distance band to separated trainig and testing folds)}
-#'     \item{species - the name of the species (column), if provided}
-#'     \item{dataType - species data type}
-#'     \item{records - a table with the number of points in each category of training and testing}
-#'     }
-#'
 #' @export
 buffering <- function(speciesData,
                       species = NULL,
@@ -72,106 +62,7 @@ buffering <- function(speciesData,
     records = out$records
   )
 
-  # ## change the sp objects to sf
-  # if(methods::is(speciesData, "SpatialPoints")){
-  #   speciesData <- sf::st_as_sf(speciesData)
-  # } else if(!methods::is(speciesData, "sf")){
-  #   stop("speciesData should be a sf or SpatialPoints object")
-  # }
-  # ## the crs must be defined
-  # if(is.na(sf::st_crs(speciesData))){
-  #   stop("The coordinate reference system of speciesData must be defined.")
-  # }
-  # ## check if species is a col in speciesData
-  # if(!is.null(species)){
-  #   if(species %in% colnames(speciesData) == FALSE){
-  #     warning("There is no match between the columns name in 'speciesData' and 'species' argument (response variable).\n")
-  #     species <- NULL
-  #   }
-  # }
-  # dmatrix <- sf::st_distance(speciesData)
-  # distuni <- dmatrix[1,1] # take the unit to avoid using units package
-  # distuni[1] <- theRange
-  # foldList <- list()
-  # if(!is.null(species)){
-  #   if(spDataType=="PB"){
-  #     unqsp <- unique(speciesData[, species, drop = TRUE])
-  #     if(!is.numeric(unqsp) || any(unqsp < 0) || any(unqsp > 1)){
-  #       stop("Presence-background (PB) type is only for species data with 0s (backgrounds) and 1s (presences).\n", "The data should be numeric.\n")
-  #     }
-  #     prI <- which(speciesData[, species, drop = TRUE] == 1) # presence indices to loop through
-  #     n <- length(prI)
-  #     cl <- sort(unique(speciesData[, species, drop = TRUE]))
-  #     clen <- length(cl)
-  #     trainTestTable <- as.data.frame(matrix(0, nrow = n, ncol = clen * 2))
-  #     names(trainTestTable) <- c(paste("train", cl, sep = "_"), paste("test", cl, sep = "_"))
-  #     if(progress==TRUE){
-  #       pb <- progress::progress_bar$new(format = " Progress [:bar] :percent in :elapsed",
-  #                                        total=n, clear=FALSE, width=75) # add progress bar
-  #     }
-  #     j <- 0
-  #     for(i in prI){ # loop through presences
-  #       j <- j + 1
-  #       trainSet <- which(dmatrix[i, ] > distuni)
-  #       if(addBG==TRUE){
-  #         test <- which(dmatrix[i, ] <= distuni)
-  #         inside <- speciesData[test, ]
-  #         testSet <- test[which(inside[, species, drop = TRUE] == 0)]
-  #         testSet[length(testSet) + 1] <- i
-  #       } else{
-  #         testSet <- i
-  #       }
-  #       foldList[[j]] <- assign(paste0("fold", j), list(trainSet, testSet))
-  #       countrain <- table(speciesData[trainSet ,species, drop = TRUE])
-  #       countest <- table(speciesData[testSet ,species, drop = TRUE])
-  #       trainTestTable[j, which(cl %in% names(countrain))] <- countrain
-  #       trainTestTable[j, clen + which(cl %in% names(countest))] <- countest
-  #       if(progress==TRUE){
-  #         pb$tick() # update progress bar
-  #       }
-  #     }
-  #   } else if(spDataType=="PA"){
-  #     n <- nrow(speciesData)
-  #     cl <- sort(unique(speciesData[, species, drop = TRUE]))
-  #     clen <- length(cl)
-  #     trainTestTable <- as.data.frame(matrix(0, nrow = n, ncol = clen * 2))
-  #     names(trainTestTable) <- c(paste("train", cl, sep = "_"), paste("test", cl, sep = "_"))
-  #     if(progress==TRUE){
-  #       pb <- progress::progress_bar$new(format = " Progress [:bar] :percent in :elapsed",
-  #                                        total=n, clear=FALSE, width=75) # add progress bar
-  #     }
-  #     for(i in seq_len(n)){
-  #       trainSet <- which(dmatrix[i, ] > distuni)
-  #       testSet <- i
-  #       foldList[[i]] <- assign(paste0("fold", i), list(trainSet, testSet))
-  #       countrain <- table(speciesData[trainSet ,species, drop = TRUE])
-  #       countest <- table(speciesData[testSet ,species, drop = TRUE])
-  #       trainTestTable[i, which(cl %in% names(countrain))] <- countrain
-  #       trainTestTable[i, clen + which(cl %in% names(countest))] <- countest
-  #       if(progress==TRUE){
-  #         pb$tick() # update progress bar
-  #       }
-  #     }
-  #   }
-  # } else{ # data with no species column
-  #   n <- nrow(speciesData)
-  #   trainTestTable <- base::data.frame(train=rep(0, n), test=0)
-  #   if(progress==TRUE){
-  #     pb <- progress::progress_bar$new(format = " Progress [:bar] :percent in :elapsed",
-  #                                      total=n, clear=FALSE, width=75) # add progress bar
-  #   }
-  #   for(i in seq_len(n)){
-  #     trainSet <- which(dmatrix[i, ] > distuni)
-  #     testSet <- i
-  #     foldList[[i]] <- assign(paste0("fold", i), list(trainSet, testSet))
-  #     trainTestTable$train[i] <- length(trainSet)
-  #     trainTestTable$test[i] <- length(testSet)
-  #     if(progress==TRUE){
-  #       pb$tick() # update progress bar
-  #     }
-  #   }
-  # }
-  # theList <- list(folds=foldList, k=n, species=species, range=theRange, dataType=spDataType, records=trainTestTable)
+ 
   class(theList) <- c("BufferedBlock")
   return(theList)
 }
--- R/environBlock.R
+++ R/environBlock.R
@@ -17,15 +17,6 @@
 #'
 #' @seealso \code{\link{cv_cluster}}
 #'
-#' @return An object of class S3. A list of objects including:
-#'    \itemize{
-#'     \item{folds - a list containing the folds. Each fold has two vectors with the training (first) and testing (second) indices}
-#'     \item{foldID - a vector of values indicating the number of the fold for each observation (each number corresponds to the same point in species data)}
-#'     \item{biomodTable - a matrix with the folds to be used in \pkg{biomod2} package}
-#'     \item{k - number of the folds}
-#'     \item{species - the name of the species (column), if provided}
-#'     \item{records - a table with the number of points in each category of training and testing}
-#'     }
 #' @export
 envBlock <- function(rasterLayer,
                      speciesData,
@@ -96,103 +87,6 @@ envBlock <- function(rasterLayer,
   )
 
 
-  # ## change the sp objects to sf
-  # if(methods::is(speciesData, "SpatialPoints")){
-  #   speciesData <- sf::st_as_sf(speciesData)
-  # } else if(!methods::is(speciesData, "sf")){
-  #   stop("speciesData should be a sf or SpatialPoints object")
-  # }
-  # ## check if species is a col in speciesData
-  # if(!is.null(species)){
-  #   if(species %in% colnames(speciesData) == FALSE){
-  #     warning("There is no match between the columns name in 'speciesData' and 'species' argument (the response variable).\n")
-  #     species <- NULL
-  #   }
-  # }
-  # if(methods::is(rasterLayer, 'Raster')){
-  #   if(raster::nlayers(rasterLayer) >= 1){
-  #     foldList <- list()
-  #     foldNum <- rep(NA, nrow(speciesData))
-  #     biomodTable <- data.frame(RUN1=rep(TRUE, nrow(speciesData)))
-  #     if(standardization=="standard"){
-  #       rasterLayer <- standardize(rasterLayer)
-  #     } else if(standardization=="normal"){
-  #       rasterLayer <- normalize(rasterLayer)
-  #     } else if(standardization=="none" || is.null(standardization)){
-  #       rasterLayer <- rasterLayer
-  #     }
-  #     if(rasterBlock==TRUE){
-  #       ext <- raster::extract(rasterLayer, speciesData)
-  #       if(anyNA(ext)){
-  #         stop("The input raster layer does not cover all the species points.")
-  #       }
-  #       # check number of raster cells
-  #       if(raster::ncell(rasterLayer) < 10 * sampleNumber){
-  #         rp <- length(which(!is.na(raster::values(rasterLayer[[1]]))))
-  #         if(rp < sampleNumber){
-  #           sampleNumber <- rp
-  #           message("The sampleNumber reduced to ", sampleNumber, "; the total number of available cells.\n")
-  #         }
-  #       }
-  #       sampr <- raster::sampleRandom(rasterLayer, size = sampleNumber)
-  #       sampr <- sampr[stats::complete.cases(sampr), ]
-  #       sampr <- rbind(ext, sampr)
-  #       kms <- stats::kmeans(sampr, centers = k, iter.max = 500, nstart = 25)
-  #       speciesData$fold <- kms$cluster[seq_len(nrow(speciesData))]
-  #     } else{
-  #       ext <- raster::extract(rasterLayer, speciesData)
-  #       if(anyNA(ext)){
-  #         stop("The input raster layer does not cover all the species points.")
-  #       }
-  #       kms <- stats::kmeans(ext, centers = k, iter.max = 500, nstart = 25)
-  #       speciesData$fold <- kms$cluster
-  #     }
-  #     if(is.null(species)){
-  #       trainTestTable <- data.frame(train = rep(0, k), test = 0)
-  #     } else{
-  #       cl <- sort(unique(speciesData[, species, drop = TRUE]))
-  #       clen <- length(cl)
-  #       trainTestTable <- as.data.frame(matrix(0, nrow = k, ncol = clen * 2))
-  #       names(trainTestTable) <- c(paste("train", cl, sep = "_"), paste("test", cl, sep = "_"))
-  #     }
-  #     for(i in seq_len(k)){
-  #       testSet <- which(speciesData$fold == i)
-  #       trainSet <- which(speciesData$fold != i)
-  #       foldNum[testSet] <- i
-  #       foldList[[i]] <- assign(paste0("fold", i), list(trainSet, testSet))
-  #       if(is.null(species)){
-  #         trainTestTable$train[i] <- length(trainSet)
-  #         trainTestTable$test[i] <- length(testSet)
-  #       } else{
-  #         countrain <- table(speciesData[trainSet ,species, drop = TRUE])
-  #         countest <- table(speciesData[testSet ,species, drop = TRUE])
-  #         trainTestTable[i, which(cl %in% names(countrain))] <- countrain
-  #         trainTestTable[i, clen + which(cl %in% names(countest))] <- countest
-  #       }
-  #       if(biomod2Format==TRUE){ # creating a biomod2 DataSplitTable for validation
-  #         colm <- paste0("RUN", i)
-  #         biomodTable[,colm] <- FALSE
-  #         biomodTable[trainSet, colm] <- TRUE
-  #       }
-  #     }
-  #     if(any(trainTestTable <= numLimit)){
-  #       zerofolds <- which(apply(trainTestTable, 1, function(x) any(x <= numLimit)))
-  #       if(length(zerofolds) > 1){
-  #         warning("The folds ", paste(zerofolds, collapse = ", "), " have class(es) with ", numLimit, " or less records")
-  #       } else{
-  #         warning("The fold ", zerofolds, " has class(es) with ", numLimit, " or less records")
-  #       }
-  #     }
-  #     if(biomod2Format==TRUE){
-  #       biomodTable <- as.matrix(biomodTable)
-  #       theList <- list(folds=foldList, foldID=foldNum, biomodTable=biomodTable, k=k, species=species, records=trainTestTable)
-  #     } else{
-  #       theList <- list(folds=foldList, foldID=foldNum, biomodTable=NULL, k=k, species=species, records=trainTestTable)
-  #     }
-  #   } else stop("'The raster layer is empty!'")
-  # } else stop('The input file is not a valid R raster file')
-  # if(verbose) print(trainTestTable)
-  # specify the output class
   class(theList) <- c("EnvironmentalBlock")
   return(theList)
 }
