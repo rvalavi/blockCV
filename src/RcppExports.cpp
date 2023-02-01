d84d7c12775246037a63013adf6a274991ac2a0c
Ian Flint <iflint@2200l-154784-l.mobility.unimelb.net.au>
2023-02-01 19:38:39 +1100

    Improve nndm speed

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,26 +11,22 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // nndm_cpp
-Rcpp::NumericMatrix nndm_cpp(Rcpp::NumericMatrix X, Rcpp::NumericVector Gjstar, Rcpp::NumericVector Gij, double rmin, double phi, int imin, int jmin, double min_train);
-RcppExport SEXP _blockCV_nndm_cpp(SEXP XSEXP, SEXP GjstarSEXP, SEXP GijSEXP, SEXP rminSEXP, SEXP phiSEXP, SEXP iminSEXP, SEXP jminSEXP, SEXP min_trainSEXP) {
+Rcpp::NumericMatrix nndm_cpp(NumericMatrix X, NumericVector Gij, double phi, double min_train);
+RcppExport SEXP _blockCV_nndm_cpp(SEXP XSEXP, SEXP GijSEXP, SEXP phiSEXP, SEXP min_trainSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type X(XSEXP);
-    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type Gjstar(GjstarSEXP);
-    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type Gij(GijSEXP);
-    Rcpp::traits::input_parameter< double >::type rmin(rminSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type Gij(GijSEXP);
     Rcpp::traits::input_parameter< double >::type phi(phiSEXP);
-    Rcpp::traits::input_parameter< int >::type imin(iminSEXP);
-    Rcpp::traits::input_parameter< int >::type jmin(jminSEXP);
     Rcpp::traits::input_parameter< double >::type min_train(min_trainSEXP);
-    rcpp_result_gen = Rcpp::wrap(nndm_cpp(X, Gjstar, Gij, rmin, phi, imin, jmin, min_train));
+    rcpp_result_gen = Rcpp::wrap(nndm_cpp(X, Gij, phi, min_train));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_blockCV_nndm_cpp", (DL_FUNC) &_blockCV_nndm_cpp, 8},
+    {"_blockCV_nndm_cpp", (DL_FUNC) &_blockCV_nndm_cpp, 4},
     {NULL, NULL, 0}
 };
 
