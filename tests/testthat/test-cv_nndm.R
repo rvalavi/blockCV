675e547784f1f0dc2336de5bc49114ff26e8951d
Roozbeh Valavi <val085@csiro.au>
2025-08-14 15:39:21 +1000

    some formatting

--- tests/testthat/test-cv_nndm.R
+++ tests/testthat/test-cv_nndm.R
@@ -10,131 +10,131 @@ expect_names <- c(
 
 
 aus <- system.file("extdata/au/", package = "blockCV") |>
-  list.files(full.names = TRUE) |>
-  terra::rast()
+    list.files(full.names = TRUE) |>
+    terra::rast()
 
 pa_data <- read.csv(system.file("extdata/", "species.csv", package = "blockCV")) |>
-  sf::st_as_sf(coords = c("x", "y"), crs = 7845)
+    sf::st_as_sf(coords = c("x", "y"), crs = 7845)
 pa_data <- pa_data[1:100, ]
 
 test_that("test that cv_nndm function works properly with presence-absence data",
           {
-            bloo <- cv_nndm(
-              x = pa_data,
-              column = "occ",
-              r = aus,
-              size = 250000,
-              num_sample = 3000,
-              presence_bg = FALSE
-            )
+              bloo <- cv_nndm(
+                  x = pa_data,
+                  column = "occ",
+                  r = aus,
+                  size = 250000,
+                  num_sample = 3000,
+                  presence_bg = FALSE
+              )
 
-            expect_true(exists("bloo"))
-            expect_s3_class(bloo, "cv_nndm")
-            expect_equal(names(bloo), expect_names)
-            expect_equal(length(bloo$folds), nrow(pa_data))
-            expect_type(bloo$folds, "list")
-            expect_type(bloo$k, "integer")
-            expect_type(bloo$column, "character")
-            expect_type(bloo$size, "double")
-            expect_equal(bloo$presence_bg, FALSE)
-            expect_equal(dim(bloo$records), c(nrow(pa_data), 4))
-            expect_true(!all(bloo$records == 0))
+              expect_true(exists("bloo"))
+              expect_s3_class(bloo, "cv_nndm")
+              expect_equal(names(bloo), expect_names)
+              expect_equal(length(bloo$folds), nrow(pa_data))
+              expect_type(bloo$folds, "list")
+              expect_type(bloo$k, "integer")
+              expect_type(bloo$column, "character")
+              expect_type(bloo$size, "double")
+              expect_equal(bloo$presence_bg, FALSE)
+              expect_equal(dim(bloo$records), c(nrow(pa_data), 4))
+              expect_true(!all(bloo$records == 0))
 
           })
 
 
 test_that("test that cv_nndm function works properly with presence-background data",
           {
-            bloo <- cv_nndm(
-              x = pa_data,
-              column = "occ",
-              r = aus,
-              size = 250000,
-              sampling = "regular",
-              presence_bg = TRUE
-            )
+              bloo <- cv_nndm(
+                  x = pa_data,
+                  column = "occ",
+                  r = aus,
+                  size = 250000,
+                  sampling = "regular",
+                  presence_bg = TRUE
+              )
 
-            expect_true(exists("bloo"))
-            expect_s3_class(bloo, "cv_nndm")
-            expect_equal(names(bloo), expect_names)
-            expect_equal(length(bloo$folds), sum(pa_data$occ))
-            expect_type(bloo$folds, "list")
-            expect_type(bloo$k, "integer")
-            expect_type(bloo$column, "character")
-            expect_type(bloo$size, "double")
-            expect_equal(bloo$presence_bg, TRUE)
-            expect_equal(dim(bloo$records), c(sum(pa_data$occ), 4))
-            expect_true(!all(bloo$records == 0))
+              expect_true(exists("bloo"))
+              expect_s3_class(bloo, "cv_nndm")
+              expect_equal(names(bloo), expect_names)
+              expect_equal(length(bloo$folds), sum(pa_data$occ))
+              expect_type(bloo$folds, "list")
+              expect_type(bloo$k, "integer")
+              expect_type(bloo$column, "character")
+              expect_type(bloo$size, "double")
+              expect_equal(bloo$presence_bg, TRUE)
+              expect_equal(dim(bloo$records), c(sum(pa_data$occ), 4))
+              expect_true(!all(bloo$records == 0))
 
           })
 
 test_that("test that cv_nndm function works properly with no species specified",
           {
-            bloo <- cv_nndm(
-              x = sf::as_Spatial(pa_data),
-              r = aus,
-              size = 250000,
-              num_sample = 3000,
-              plot = FALSE
-            )
+              bloo <- cv_nndm(
+                  x = sf::as_Spatial(pa_data),
+                  r = aus,
+                  size = 250000,
+                  num_sample = 3000,
+                  plot = FALSE
+              )
 
-            expect_true(exists("bloo"))
-            expect_s3_class(bloo, "cv_nndm")
-            expect_equal(names(bloo), expect_names)
-            expect_equal(length(bloo$folds), nrow(pa_data))
-            expect_type(bloo$folds, "list")
-            expect_type(bloo$k, "integer")
-            expect_null(bloo$species)
-            expect_type(bloo$size, "double")
-            expect_equal(bloo$presence_bg, FALSE)
-            expect_equal(dim(bloo$records), c(nrow(pa_data), 2))
-            expect_true(!all(bloo$records == 0))
+              expect_true(exists("bloo"))
+              expect_s3_class(bloo, "cv_nndm")
+              expect_equal(names(bloo), expect_names)
+              expect_equal(length(bloo$folds), nrow(pa_data))
+              expect_type(bloo$folds, "list")
+              expect_type(bloo$k, "integer")
+              expect_null(bloo$species)
+              expect_type(bloo$size, "double")
+              expect_equal(bloo$presence_bg, FALSE)
+              expect_equal(dim(bloo$records), c(nrow(pa_data), 2))
+              expect_true(!all(bloo$records == 0))
 
-            expect_equal(print(bloo), "cv_nndm")
-            expect_output(summary(bloo))
+              expect_equal(print(bloo), "cv_nndm")
+              expect_output(summary(bloo))
 
           })
 
 
 test_that("test cv_nndm function with no matching species column", {
-  expect_warning(
-    bloo <- cv_nndm(
-      x = pa_data,
-      column = "response",
-      r = aus,
-      size = 250000,
-      num_sample = 3000,
-      presence_bg = FALSE
+    expect_warning(
+        bloo <- cv_nndm(
+            x = pa_data,
+            column = "response",
+            r = aus,
+            size = 250000,
+            num_sample = 3000,
+            presence_bg = FALSE
+        )
     )
-  )
 
-  expect_true(exists("bloo"))
-  expect_equal(dim(bloo$records), c(nrow(pa_data), 2))
-  expect_true(!all(bloo$records == 0))
+    expect_true(exists("bloo"))
+    expect_equal(dim(bloo$records), c(nrow(pa_data), 2))
+    expect_true(!all(bloo$records == 0))
 
 })
 
 test_that("test cv_nndm function with no spatial column data", {
-  expect_error(
-    cv_nndm(
-      x = "pa_data",
-      r = aus,
-      size = 250000
+    expect_error(
+        cv_nndm(
+            x = "pa_data",
+            r = aus,
+            size = 250000
+        )
     )
-  )
 
 })
 
 test_that("test cv_nndm function to have sptial points with no CRS", {
-  sf::st_crs(pa_data) <- NA
+    sf::st_crs(pa_data) <- NA
 
-  expect_error(
-    cv_nndm(
-      x = pa_data,
-      column = "occ",
-      r = aus,
-      size = 250000
+    expect_error(
+        cv_nndm(
+            x = pa_data,
+            column = "occ",
+            r = aus,
+            size = 250000
+        )
     )
-  )
 
 })
--- tests/testthat/test-cv_spatial.R
+++ tests/testthat/test-cv_spatial.R
@@ -8,226 +8,245 @@ expect_names <- c("folds_list",
                   "records")
 
 aus <- system.file("extdata/au/", package = "blockCV") |>
-  list.files(full.names = TRUE) |>
-  terra::rast()
+    list.files(full.names = TRUE) |>
+    terra::rast()
 
 pa_data <- read.csv(system.file("extdata/", "species.csv", package = "blockCV")) |>
-  sf::st_as_sf(coords = c("x", "y"), crs = 7845)
+    sf::st_as_sf(coords = c("x", "y"), crs = 7845)
 pa_data <- pa_data[1:200, ]
 
 test_that("test cv_spatial function with random assingment and raster file", {
 
-  set.seed(1000)
-  scv <- cv_spatial(x = pa_data,
-                    column = "occ",
-                    r = aus,
-                    size = 450000,
-                    k = 5,
-                    selection = "random",
-                    iteration = 5,
-                    biomod2 = TRUE,
-                    offset = c(0.3, 0.2),
-                    plot = FALSE,
-                    progress = TRUE)
+    set.seed(1000)
+    scv <- cv_spatial(
+        x = pa_data,
+        column = "occ",
+        r = aus,
+        size = 450000,
+        k = 5,
+        selection = "random",
+        iteration = 5,
+        biomod2 = TRUE,
+        offset = c(0.3, 0.2),
+        plot = FALSE,
+        progress = TRUE
+    )
 
-  expect_true(exists("scv"))
-  expect_s3_class(scv, "cv_spatial")
-  expect_equal(names(scv), expect_names)
-  expect_equal(length(scv$folds_list), 5)
-  expect_type(scv$folds_list, "list")
-  expect_type(scv$biomod_table, "logical")
-  expect_equal(dim(scv$biomod_table), c(nrow(pa_data), 5))
-  expect_equal(scv$k, 5)
-  expect_s3_class(scv$blocks, "sf")
-  expect_type(scv$column, "character")
-  expect_type(scv$size, "double")
-  expect_equal(dim(scv$records), c(5, 4))
-  expect_true(
-    !all(scv$records == 0)
-  )
+    expect_true(exists("scv"))
+    expect_s3_class(scv, "cv_spatial")
+    expect_equal(names(scv), expect_names)
+    expect_equal(length(scv$folds_list), 5)
+    expect_type(scv$folds_list, "list")
+    expect_type(scv$biomod_table, "logical")
+    expect_equal(dim(scv$biomod_table), c(nrow(pa_data), 5))
+    expect_equal(scv$k, 5)
+    expect_s3_class(scv$blocks, "sf")
+    expect_type(scv$column, "character")
+    expect_type(scv$size, "double")
+    expect_equal(dim(scv$records), c(5, 4))
+    expect_true(
+        !all(scv$records == 0)
+    )
 
 })
 
 
 test_that("test cv_spatial function with systematic assingment and no raster file", {
 
-  scv <- cv_spatial(x = sf::as_Spatial(pa_data),
-                    rows_cols = c(10, 10),
-                    k = 5,
-                    selection = "systematic",
-                    biomod2 = FALSE,
-                    plot = TRUE)
+    scv <- cv_spatial(
+        x = sf::as_Spatial(pa_data),
+        rows_cols = c(10, 10),
+        k = 5,
+        selection = "systematic",
+        biomod2 = FALSE,
+        plot = TRUE
+    )
 
-  expect_true(exists("scv"))
-  expect_s3_class(scv, "cv_spatial")
-  expect_equal(names(scv), expect_names)
-  expect_equal(length(scv$folds_list), 5)
-  expect_type(scv$folds_list, "list")
-  expect_null(scv$biomod_table, "matrix")
-  expect_equal(scv$k, 5)
-  expect_s3_class(scv$blocks, "sf")
-  expect_null(scv$column)
-  expect_null(scv$size)
-  expect_equal(dim(scv$records), c(5, 2))
-  expect_true(
-    !all(scv$records == 0)
-  )
+    expect_true(exists("scv"))
+    expect_s3_class(scv, "cv_spatial")
+    expect_equal(names(scv), expect_names)
+    expect_equal(length(scv$folds_list), 5)
+    expect_type(scv$folds_list, "list")
+    expect_null(scv$biomod_table, "matrix")
+    expect_equal(scv$k, 5)
+    expect_s3_class(scv$blocks, "sf")
+    expect_null(scv$column)
+    expect_null(scv$size)
+    expect_equal(dim(scv$records), c(5, 2))
+    expect_true(
+        !all(scv$records == 0)
+    )
 
-  expect_equal(print(scv), "cv_spatial")
-  expect_message(plot(scv))
-  expect_output(summary(scv))
+    expect_equal(print(scv), "cv_spatial")
+    expect_message(plot(scv))
+    expect_output(summary(scv))
 
 })
 
 
 test_that("test cv_spatial function with non-numeric iteration", {
 
-  scv <- cv_spatial(x = pa_data,
-                    hexagon = FALSE,
-                    k = 5,
-                    selection = "random",
-                    iteration = "2", # not numeric
-                    biomod2 = FALSE,
-                    plot = FALSE)
+    scv <- cv_spatial(
+        x = pa_data,
+        hexagon = FALSE,
+        k = 5,
+        selection = "random",
+        iteration = "2", # not numeric
+        biomod2 = FALSE,
+        plot = FALSE
+    )
 
-  expect_true(exists("scv"))
-  expect_s3_class(scv, "cv_spatial")
-  expect_equal(names(scv), expect_names)
-  expect_equal(length(scv$folds_list), 5)
-  expect_type(scv$folds_list, "list")
-  expect_null(scv$biomod_table, "matrix")
-  expect_equal(scv$k, 5)
-  expect_s3_class(scv$blocks, "sf")
-  expect_null(scv$column)
-  expect_null(scv$size)
-  expect_equal(dim(scv$records), c(5, 2))
-  expect_true(
-    !all(scv$records == 0)
-  )
+    expect_true(exists("scv"))
+    expect_s3_class(scv, "cv_spatial")
+    expect_equal(names(scv), expect_names)
+    expect_equal(length(scv$folds_list), 5)
+    expect_type(scv$folds_list, "list")
+    expect_null(scv$biomod_table, "matrix")
+    expect_equal(scv$k, 5)
+    expect_s3_class(scv$blocks, "sf")
+    expect_null(scv$column)
+    expect_null(scv$size)
+    expect_equal(dim(scv$records), c(5, 2))
+    expect_true(
+        !all(scv$records == 0)
+    )
 
-  expect_equal(print(scv), "cv_spatial")
-  expect_message(plot(scv))
-  expect_output(summary(scv))
+    expect_equal(print(scv), "cv_spatial")
+    expect_message(plot(scv))
+    expect_output(summary(scv))
 
 })
 
 test_that("test cv_spatial with checkerboard assingment and only row blocks", {
 
-  scv <- cv_spatial(x = sf::as_Spatial(pa_data),
-                    hexagon = FALSE,
-                    rows_cols = c(5, 1),
-                    selection = "checkerboard",
-                    biomod2 = FALSE,
-                    plot = TRUE)
+    scv <- cv_spatial(
+        x = sf::as_Spatial(pa_data),
+        hexagon = FALSE,
+        rows_cols = c(5, 1),
+        selection = "checkerboard",
+        biomod2 = FALSE,
+        plot = TRUE
+    )
 
-  expect_true(exists("scv"))
-  expect_s3_class(scv, "cv_spatial")
-  expect_equal(names(scv), expect_names)
-  expect_equal(length(scv$folds_list), 2)
-  expect_type(scv$folds_list, "list")
-  expect_null(scv$biomod_table)
-  expect_equal(scv$k, 2)
-  expect_s3_class(scv$blocks, "sf")
-  expect_null(scv$column)
-  expect_null(scv$size)
-  expect_equal(dim(scv$records), c(2, 2))
-  expect_true(
-    !all(scv$records == 0)
-  )
+    expect_true(exists("scv"))
+    expect_s3_class(scv, "cv_spatial")
+    expect_equal(names(scv), expect_names)
+    expect_equal(length(scv$folds_list), 2)
+    expect_type(scv$folds_list, "list")
+    expect_null(scv$biomod_table)
+    expect_equal(scv$k, 2)
+    expect_s3_class(scv$blocks, "sf")
+    expect_null(scv$column)
+    expect_null(scv$size)
+    expect_equal(dim(scv$records), c(2, 2))
+    expect_true(
+        !all(scv$records == 0)
+    )
 
-  expect_equal(print(scv), "cv_spatial")
-  expect_message(plot(scv))
-  expect_output(summary(scv))
+    expect_equal(print(scv), "cv_spatial")
+    expect_message(plot(scv))
+    expect_output(summary(scv))
 
 })
 
 
 test_that("test cv_spatial with user-defined blocks", {
 
-  # make a spatial block polygon
-  user_poly <- .make_blocks(x_obj = pa_data, blocksize = 450000)
+    # make a spatial block polygon
+    user_poly <- .make_blocks(x_obj = pa_data, blocksize = 450000)
 
-  scv <- cv_spatial(x = pa_data,
-                    user_blocks = user_poly,
-                    selection = "random",
-                    iteration = 5,
-                    biomod2 = FALSE,
-                    progress = FALSE,
-                    plot = FALSE)
+    scv <- cv_spatial(
+        x = pa_data,
+        user_blocks = user_poly,
+        selection = "random",
+        iteration = 5,
+        biomod2 = FALSE,
+        progress = FALSE,
+        plot = FALSE
+    )
 
-  expect_true(exists("scv"))
-  expect_s3_class(scv, "cv_spatial")
-  expect_equal(names(scv), expect_names)
-  expect_equal(length(scv$folds_list), 5)
-  expect_type(scv$folds_list, "list")
-  expect_null(scv$biomod_table)
-  expect_equal(scv$k, 5)
-  expect_s3_class(scv$blocks, "sf")
-  expect_null(scv$column)
-  expect_null(scv$size)
-  expect_equal(dim(scv$records), c(5, 2))
-  expect_true(
-    !all(scv$records == 0)
-  )
+    expect_true(exists("scv"))
+    expect_s3_class(scv, "cv_spatial")
+    expect_equal(names(scv), expect_names)
+    expect_equal(length(scv$folds_list), 5)
+    expect_type(scv$folds_list, "list")
+    expect_null(scv$biomod_table)
+    expect_equal(scv$k, 5)
+    expect_s3_class(scv$blocks, "sf")
+    expect_null(scv$column)
+    expect_null(scv$size)
+    expect_equal(dim(scv$records), c(5, 2))
+    expect_true(
+        !all(scv$records == 0)
+    )
 
-  expect_equal(print(scv), "cv_spatial")
-  expect_message(plot(scv))
-  expect_output(summary(scv))
+    expect_equal(print(scv), "cv_spatial")
+    expect_message(plot(scv))
+    expect_output(summary(scv))
 
 })
 
 
 test_that("test cv_spatial failur: number of blocks, wrong selection", {
 
-  expect_error(cv_spatial(x = pa_data,
-                          rows_cols = c(2, 2),
-                          hexagon = FALSE,
-                          k = 15, # very high k
-                          selection = "random"))
+    expect_error(cv_spatial(
+        x = pa_data,
+        rows_cols = c(2, 2),
+        hexagon = FALSE,
+        k = 15,
+        # very high k
+        selection = "random"
+    ))
 
-  expect_error(cv_spatial(x = pa_data,
-                          k = 1, # very low k
-                          selection = "random"))
+    expect_error(cv_spatial(
+        x = pa_data,
+        k = 1,
+        # very low k
+        selection = "random"
+    ))
 
-  expect_error(cv_spatial(x = pa_data,
-                          k = 5,
-                          selection = "chance")) # wrong selection
+    expect_error(cv_spatial(x = pa_data, k = 5, selection = "chance")) # wrong selection
 
 })
 
 
 test_that("test cv_spatial failur: wrong user-defined blocks", {
 
-  expect_error(
-    cv_spatial(x = pa_data, # wrong user-defined blocks
-               column = "occ",
-               r = aus,
-               user_blocks = data.frame(a = 1),
-               size = 450000,
-               k = 5)
-  )
+    expect_error(
+        cv_spatial(
+            x = pa_data,
+            # wrong user-defined blocks
+            column = "occ",
+            r = aus,
+            user_blocks = data.frame(a = 1),
+            size = 450000,
+            k = 5
+        )
+    )
 
 })
 
 
 test_that("test cv_spatial with no speceis column match", {
 
-  expect_warning(
-    scv <- cv_spatial(x = pa_data,
-                      column = "response", # wrong response
-                      r = aus,
-                      size = 450000,
-                      k = 5)
-  )
 
-  expect_true(exists("scv"))
-  expect_s3_class(scv, "cv_spatial")
-  expect_equal(names(scv), expect_names)
-  expect_equal(length(scv$folds_list), 5)
-  expect_equal(scv$k, 5)
-  expect_equal(dim(scv$records), c(5, 2))
-  expect_true(
-    !all(scv$records == 0)
-  )
+    expect_warning(scv <- cv_spatial(
+        x = pa_data,
+        column = "response",
+        # wrong response
+        r = aus,
+        size = 450000,
+        k = 5
+    ))
+
+    expect_true(exists("scv"))
+    expect_s3_class(scv, "cv_spatial")
+    expect_equal(names(scv), expect_names)
+    expect_equal(length(scv$folds_list), 5)
+    expect_equal(scv$k, 5)
+    expect_equal(dim(scv$records), c(5, 2))
+    expect_true(
+        !all(scv$records == 0)
+    )
 
 })
